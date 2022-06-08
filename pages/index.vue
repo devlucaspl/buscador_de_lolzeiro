e49b2201e835d8181e44c2e8013b42d9ce894508
Lucas Pereira Lopes <84463328+devlucaspl@users.noreply.github.com>
2022-06-08 16:45:14 -0300

    Adicionando o axios e criando a função de buscar

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,37 @@
 <template>
   <div class="flex flex-col items-center">
-    <h1 class="mt-10">Buscador de Lolzeiro</h1>
-    <div class="flex align-middle">
+    <h1 class="mt-10 mb-4 text-lg font-medium">Buscador de Lolzeiro</h1>
+    <div class="flex flex-row gap-2 items-end">
       <div>
-        <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
         <div class="mt-1">
-          <input type="email" name="email" id="email"
-            class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-            placeholder="you@example.com" />
+          <input type="text" name="player" id="player" v-model="nome_do_player"
+            class="h-8 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md"/>
         </div>
       </div>
-      <button type="button" class="h-min inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Button text</button>
+      <button type="button" @click="buscar_player()" class="h-8 inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Encontrar</button>
     </div>
   </div>
 </template>
 
 <script>
+
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data() {
+    return {
+      nome_do_player: '',
+    };
+  },
+  methods: {
+    async buscar_player(){
+      console.log(this.nome_do_player)
+      APICallString = 'https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + this.nome_do_player + '?api_key='+ API_KEY;
+      try {
+        const { data } = await axios.get(APICallString)
+      } catch (e) {
+        alert('erro ao buscar')
+      }
+    }
+  }
 }
 </script>
